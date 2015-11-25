f11e332d3573d71d521bf4717742112125ad745d
João Victor <victorbarboza@outlook.com>
2015-11-24 23:54:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-    char nome_da_foto[100], tipo[2], comando[20], nome_da_foto_sppm[100];
+    char nome_da_foto[100], tipo[2], comando[20], nome_da_foto_sppm[100], nome_da_foto_final[100];
     int alt, lar, qua, rot;
 	FILE *foto;
+	
     typedef struct {
         int r;
         int g;
@@ -11,10 +12,8 @@
     }RGB;
 
 #include "funcoes.h"
-#include "efeitos.h"
+#include "filtros.h"
 
-
-//FALTA: colocar para aceitar somente o tipo P3
 int main (void){
     entrada();
     RGB px[alt][lar];
