bfe1dedfcfe060bca2fde99d33998b93028c9df6
João Victor <victorbarboza@outlook.com>
2015-11-26 02:30:03 -0300

    Update filtros.h

--- filtros.h
+++ filtros.h
@@ -21,18 +21,18 @@ void thr(RGB px[alt][lar]){
 	strcat(nome_da_foto_final, "_thr.ppm");
 	saida(edit);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void rot_aux(RGB px[alt][lar]){
-	printf("Insira o grau de rotação (90, 180 ou 270): ");
+	printf("Insira o grau de rotaÃ§Ã£o (90, 180 ou 270): ");
 	scanf("%i", &rot);
-	
+
 	while(rot != 90 && rot != 180 && rot != 270){
-		printf("Insira o grau de rotação (90, 180 ou 270): ");
+		printf("Insira o grau de rotaÃ§Ã£o (90, 180 ou 270): ");
 		scanf("%i", &rot);
 	}
-	
+
 	if(rot==90) rot_90(px);
 	if(rot==180) rot_180(px);
 	if(rot==270) rot_270(px);
@@ -41,7 +41,7 @@ void rot_aux(RGB px[alt][lar]){
 void rot_180(RGB px[alt][lar]){
 	int l, c;
     RGB edit[lar][alt];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
             edit[alt-c-1][l].r=px[l][c].r;
@@ -49,7 +49,7 @@ void rot_180(RGB px[alt][lar]){
             edit[alt-c-1][l].b=px[l][c].b;
         }
     }
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
             px[alt-c-1][l].r=edit[l][c].r;
@@ -61,13 +61,13 @@ void rot_180(RGB px[alt][lar]){
 	strcat(nome_da_foto_final, "_rot.ppm");
     saida(px);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);    
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void rot_270(RGB px[alt][lar]){
 	int l, c;
     RGB edit[lar][alt];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
             edit[alt-c-1][l].r=px[l][c].r;
@@ -89,11 +89,11 @@ void rot_270(RGB px[alt][lar]){
             edit[alt-c-1][l].b=px[l][c].b;
         }
     }
-    
+
     int temp=alt;
     alt=lar;
     lar=temp;
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
             px[l][c].r=edit[l][c].r;
@@ -101,18 +101,18 @@ void rot_270(RGB px[alt][lar]){
             px[l][c].b=edit[l][c].b;
         }
     }
-    
+
 	strcpy (nome_da_foto_final, nome_da_foto_sppm);
 	strcat(nome_da_foto_final, "_rot.ppm");
     saida(px);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);    
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void rot_90(RGB px[alt][lar]){
 	int l, c;
     RGB edit[lar][alt];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
             edit[alt-c-1][l].r=px[l][c].r;
@@ -127,22 +127,22 @@ void rot_90(RGB px[alt][lar]){
             px[l][c].b=edit[l][c].b;
         }
     }
-    
+
     int temp=alt;
     alt=lar;
     lar=temp;
-    
+
 	strcpy (nome_da_foto_final, nome_da_foto_sppm);
 	strcat(nome_da_foto_final, "_rot.ppm");
     saida(px);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);    
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void blu(RGB px[alt][lar]){
 	int l, c;
     RGB edit[alt][lar];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
 			if(l==0 && c==0){
@@ -197,13 +197,13 @@ void blu(RGB px[alt][lar]){
 	strcat(nome_da_foto_final, "_blu.ppm");
 	saida(edit);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void sha(RGB px[alt][lar]){
 	int l, c;
     RGB edit[alt][lar];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
 			if(l==0 && c==0){
@@ -214,7 +214,7 @@ void sha(RGB px[alt][lar]){
 			else if(l==alt-1 && c==0){
 				edit[l][c].r=px[l-1][c].r*(-1)+px[l][c].r*(5)+px[l][c+1].r*(-1);
 				edit[l][c].g=px[l-1][c].g*(-1)+px[l][c].g*(5)+px[l][c+1].g*(-1);
-				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1);			
+				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1);
 			}
 			else if(l==alt-1 && c==lar-1){
 				edit[l][c].r=px[l-1][c].r*(-1)+px[l][c-1].r*(-1)+px[l][c].r*(5);
@@ -229,17 +229,17 @@ void sha(RGB px[alt][lar]){
 			else if(l==0){
 				edit[l][c].r=px[l][c-1].r*(-1)+px[l][c].r*(5)+px[l][c+1].r*(-1)+px[l+1][c].r*(-1);
 				edit[l][c].g=px[l][c-1].g*(-1)+px[l][c].g*(5)+px[l][c+1].g*(-1)+px[l+1][c].g*(-1);
-				edit[l][c].b=px[l][c-1].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1)+px[l+1][c].b*(-1);		
+				edit[l][c].b=px[l][c-1].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1)+px[l+1][c].b*(-1);
 			}
 			else if(l==alt-1){
 				edit[l][c].r=px[l-1][c].r*(-1)+px[l][c-1].r*(-1)+px[l][c].r*(5)+px[l][c+1].r*(-1);
 				edit[l][c].g=px[l-1][c].g*(-1)+px[l][c-1].g*(-1)+px[l][c].g*(5)+px[l][c+1].g*(-1);
-				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c-1].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1);			
+				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c-1].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1);
 			}
 			else if(c==0){
 				edit[l][c].r=px[l-1][c].r*(-1)+px[l][c].r*(5)+px[l][c+1].r*(-1)+px[l+1][c].r*(-1);
 				edit[l][c].g=px[l-1][c].g*(-1)+px[l][c].g*(5)+px[l][c+1].g*(-1)+px[l+1][c].g*(-1);
-				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1)+px[l+1][c].b*(-1);				
+				edit[l][c].b=px[l-1][c].b*(-1)+px[l][c].b*(5)+px[l][c+1].b*(-1)+px[l+1][c].b*(-1);
 			}
 			else if(c==lar-1){
 				edit[l][c].r=px[l-1][c].r*(-1)+px[l][c-1].r*(-1)+px[l][c].r*(5)+px[l+1][c].r*(-1);
@@ -263,13 +263,13 @@ void sha(RGB px[alt][lar]){
 	strcat(nome_da_foto_final, "_sha.ppm");
 	saida(edit);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
 void bor(RGB px[alt][lar]){
 	int l, c;
     RGB edit[alt][lar];
-    
+
     for(l=0; l < alt; l++){
         for(c=0; c < lar; c++){
 			if(l==0 && c==0){
@@ -280,7 +280,7 @@ void bor(RGB px[alt][lar]){
 			else if(l==alt-1 && c==0){
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c].r*(-4)+px[l][c+1].r*(1);
 				edit[l][c].g=px[l-1][c].g*(1)+px[l][c].g*(-4)+px[l][c+1].g*(1);
-				edit[l][c].b=px[l-1][c].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1);			
+				edit[l][c].b=px[l-1][c].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1);
 			}
 			else if(l==alt-1 && c==lar-1){
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c-1].r*(1)+px[l][c].r*(-4);
@@ -295,8 +295,8 @@ void bor(RGB px[alt][lar]){
 			else if(l==0){
 				edit[l][c].r=px[l][c-1].r*(1)+px[l][c].r*(-4)+px[l][c+1].r*(1)+px[l+1][c].r*(1);
 				edit[l][c].g=px[l][c-1].g*(1)+px[l][c].g*(-4)+px[l][c+1].g*(1)+px[l+1][c].g*(1);
-				edit[l][c].b=px[l][c-1].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1)+px[l+1][c].b*(1);		
-			
+				edit[l][c].b=px[l][c-1].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1)+px[l+1][c].b*(1);
+
 			}
 			else if(l==alt-1){
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c-1].r*(1)+px[l][c].r*(-4)+px[l][c+1].r*(1);
@@ -306,12 +306,12 @@ void bor(RGB px[alt][lar]){
 			else if(c==0){
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c].r*(-4)+px[l][c+1].r*(1)+px[l+1][c].r*(1);
 				edit[l][c].g=px[l-1][c].g*(1)+px[l][c].g*(-4)+px[l][c+1].g*(1)+px[l+1][c].g*(1);
-				edit[l][c].b=px[l-1][c].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1)+px[l+1][c].b*(1);				
+				edit[l][c].b=px[l-1][c].b*(1)+px[l][c].b*(-4)+px[l][c+1].b*(1)+px[l+1][c].b*(1);
 			}
 			else if(c==lar-1){
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c-1].r*(1)+px[l][c].r*(-4)+px[l+1][c].r*(1);
 				edit[l][c].g=px[l-1][c].g*(1)+px[l][c-1].g*(1)+px[l][c].g*(-4)+px[l+1][c].g*(1);
-				edit[l][c].b=px[l-1][c].b*(1)+px[l][c-1].b*(1)+px[l][c].b*(-4)+px[l+1][c].b*(1);				
+				edit[l][c].b=px[l-1][c].b*(1)+px[l][c-1].b*(1)+px[l][c].b*(-4)+px[l+1][c].b*(1);
 			}
 			else{
 				edit[l][c].r=px[l-1][c].r*(1)+px[l][c-1].r*(1)+px[l][c].r*(-4)+px[l][c+1].r*(1)+px[l+1][c].r*(1);
@@ -330,21 +330,21 @@ void bor(RGB px[alt][lar]){
 	strcat(nome_da_foto_final, "_bor.ppm");
 	saida(edit);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
-void amp(RGB px[alt][lar]){	
+void amp(RGB px[alt][lar]){
 	int x;
 	printf("Insira: ");
 	scanf("%i", &x);
 	int l, c, rl, rc;
-	
+
     FILE *saida;
 	strcpy (nome_da_foto_final, nome_da_foto_sppm);
 	strcat(nome_da_foto_final, "_amp.ppm");
     saida=fopen(nome_da_foto_final, "w");
     fprintf(saida, "%s\n%i %i\n%i\n", tipo, alt*x, lar*x, qua);
-	
+
 	for(l = 0; l < alt; l++)
 		for (rl = 0; rl < x; rl++) //repete linhas x vezes
 			for(c = 0; c < lar; c++)
@@ -352,22 +352,22 @@ void amp(RGB px[alt][lar]){
 					fprintf(saida, "%i %i %i\n", px[l][c].r, px[l][c].g, px[l][c].b);
 	fclose(saida);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
 
-void red(RGB px[alt][lar]){	
+void red(RGB px[alt][lar]){
 	int x;
 	printf("Insira o valor da reducao (multiplo dos valores de altura e largura): ");
 	scanf("%i", &x);
-	
+
 	while (alt%x!=0 && lar%x!=0){
 		printf("O valor inserido nao e multiplo dos valores de altura e largura. Tente novamente: ");
 		scanf("%i", &x);
 	}
-	
+
 	int l, c;
 	RGB edit[(alt/x)][(lar/x)];
-	
+
 	for(l = 0; l < alt; l+=x)
 		for(c = 0; c < lar; c+=x){
 			edit[(l/x)][(c/x)].r=px[l][c].r;
@@ -376,10 +376,10 @@ void red(RGB px[alt][lar]){
 		}
 	alt/=x;
 	lar/=x;
-	
+
 	strcpy (nome_da_foto_final, nome_da_foto_sppm);
 	strcat(nome_da_foto_final, "_red.ppm");
 	saida(edit);
 	system("clear");
-	printf("A imagem %s foi gerada.\n", nome_da_foto_final);	
+	printf("A imagem %s foi gerada.\n", nome_da_foto_final);
 }
