e22f9856f48e455b5ed0d625ae2ea54b1d39f464
João Victor <victorbarboza@outlook.com>
2015-11-26 01:38:00 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -4,13 +4,17 @@ void sha();
 void blu();
 void bor();
 void amp();
+void red();
 void rot_aux();
 void rot_90();
 void rot_180();
 void rot_270();
+void ran();
 
 void entrada(){
-    printf("Insira o nome do arquivo (sem a extensao): ");
+    //pega o nome da foto e checa se o arquivo existe. Caso não exista, pede para inserir novamente
+    printf("### Bem-vindo(a)! ###\n");
+    printf("Insira o nome da imagem a ser modificada (sem a extensao): ");
     
     setbuf(stdin, 0);
     scanf("%s", nome_da_foto);
@@ -33,10 +37,12 @@ void entrada(){
 		foto = fopen(nome_da_foto, "r");
     }
     fscanf(foto, "%s %i %i %i", tipo, &alt, &lar, &qua);
+    alt_s=alt; //salvando para que a altura e largura da imagem original seja restaurada após o filtro de reduçao
+	lar_s=lar;
 }
 
 void processamento(RGB px[alt][lar]){
-    //pegando ls com informações de RGB de cada px
+    //pegando colunas com informações de RGB de cada px
     int l, c;
     for (l=0; l<alt; l++){
         for(c=0; c<lar; c++){
@@ -49,7 +55,6 @@ void processamento(RGB px[alt][lar]){
 
 void comandos(RGB px[alt][lar]){
    	while(strcmp (comando, "fim") != 0){
-		system("clear");
 		printf("--------------------------------------------\n");
 		printf("|| Os comandos possiveis do programa sao: ||\n");
 		printf("||        'thr' para thresholding         ||\n");
@@ -59,6 +64,7 @@ void comandos(RGB px[alt][lar]){
 		printf("||           'amp' para ampliar           ||\n");	
 		printf("||           'red' para reduzir           ||\n");			
 		printf("||       'bor' para board detection       ||\n");
+		printf("||       'ran' para efeito aleatorio      ||\n");
 		printf("||   'outra' para escolher outra imagem   ||\n");		
 		printf("||          'fim' para finalizar          ||\n");		
 		printf("--------------------------------------------\n");
@@ -67,7 +73,7 @@ void comandos(RGB px[alt][lar]){
 		
 		// Compara se 'comando' é igual ou diferente de 'edit'.
 		// Se for igual, vai para o efeito edit, se não for, pede novamente o comando.
-		while(strcmp (comando, "thr") != 0 && strcmp (comando, "amp") != 0 && strcmp (comando, "bor") != 0 && strcmp (comando, "fim") != 0 && strcmp (comando, "rot") != 0 && strcmp (comando, "outra") != 0 && strcmp (comando, "blu") != 0 && strcmp (comando, "sha") != 0){
+		while(strcmp (comando, "thr") != 0 && strcmp (comando, "red") != 0 && strcmp (comando, "amp") != 0 && strcmp (comando, "bor") != 0 && strcmp (comando, "fim") != 0 && strcmp (comando, "rot") != 0 && strcmp (comando, "outra") != 0 && strcmp (comando, "blu") != 0 && strcmp (comando, "sha") != 0 && strcmp (comando, "ran") != 0){
 			printf("Comando inexistente. Tente novamente: ");
 			scanf("%s", comando);
 		}
@@ -89,13 +95,20 @@ void comandos(RGB px[alt][lar]){
 		if (strcmp (comando, "amp") == 0){
 			amp(px);
 		}
+		if (strcmp (comando, "red") == 0){
+			red(px);
+		}
+		if (strcmp (comando, "ran") == 0){
+			ran(px);
+		}
 		if (strcmp (comando, "outra") == 0){
 			main();
 		}
 	}
+	printf("Até logo!");
 }
-
-void saida(RGB px[alt][lar]){
+//Falta: colocar pra avisar que a foto foi gerada
+void saida(RGB px[alt][lar]){	
 	int l, c;
     FILE *saida;
     saida=fopen(nome_da_foto_final, "w");
@@ -106,6 +119,7 @@ void saida(RGB px[alt][lar]){
 			fprintf(saida, "%i %i %i\n", px[l][c].r, px[l][c].g, px[l][c].b);    
 		}
 	}
-	
 	fclose(saida);
+	alt=alt_s;
+	lar=lar_s;
 }
